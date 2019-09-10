eabb6e193bf7d13092ad27f38ade970e0a804967
markomih10 <mmihajlovic@csuchico.edu>
2019-09-09 17:13:02 -0700

    Created the cpp file

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
+        std::string occupation;
+        int number=9;
+        std::string place;
+        std::string state;
 
+
+        std::cout<<"Enter a occupation: ";
+        std::cin>>occupation;
+        std::cout<<"Enter a number: ";
+        std::cin>>number;
+        std::cout<<"Enter a place; ";
+        std::cin>>place;
+        std::cout<<"Enter a state; ";
+        std::cin>>state;
+        std::cout<<"Once upon a time, a young"<<occupation<<".He had"<<number<<"bottles of beer. After he had gotten drunk, he went to his"<<place<<"to sleep. The next day he was very"<<place<<std::endl;
   return 0;
 }
